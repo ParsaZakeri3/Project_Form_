30d5df6b1037fc11de05e97dd0c3f26ca783a9a5
Parsa <132743255+ParsaZakeri3@users.noreply.github.com>
2024-04-21 15:34:59 +0330

    Update Validation.cs
    fix and improve 
    and clean my codes

--- ProjectForm/Class/Validation.cs
+++ ProjectForm/Class/Validation.cs
@@ -16,27 +16,19 @@ namespace ProjectForm.Class
                 bool resultYear = SplitDate[0].Length == 4 && int.TryParse(SplitDate[0], out int num_year) && num_year < DateTime.Now.Year ? true : false;
                 //check Month
                 bool resultmonth = SplitDate[1].Length == 2 && int.TryParse(SplitDate[1], out int num_month) && num_month >= 1 && num_month <= 12 ? true : false;
-                if (!resultmonth)
-                {
-                    resultmonth = SplitDate[1].Length == 1 && int.TryParse(SplitDate[1], out int num) ? num >= 1 && num <= 12 : false;
-                }
+                if (!resultmonth) resultmonth = SplitDate[1].Length == 1 && int.TryParse(SplitDate[1], out int num) ? num >= 1 && num <= 12 : false;
                 //check Day
                 bool resultday = SplitDate[2].Length == 2 && int.TryParse(SplitDate[2], out int num_day) && num_day >= 1 && num_day <= 30 ? true : false;
-                if (!resultday)
-                {
-                    resultday = SplitDate[1].Length == 1 && int.TryParse(SplitDate[2], out int num) ? num >= 1 && num <= 30 : false;
-                }
+                if (!resultday) resultday = SplitDate[1].Length == 1 && int.TryParse(SplitDate[2], out int num) ? num >= 1 && num <= 30 : false;
                 //check Date
-                bool Date = resultYear && resultmonth && resultday ? true : false;
-                return Date;
+                return resultYear && resultmonth && resultday ? true : false;
             }
             return false;
         }
 
         public static bool CheckTryAgain(this string TryAgainForm)
         {
-            bool result = int.TryParse(TryAgainForm, out int numtryagainform) && numtryagainform >= 1 && numtryagainform <= 2 ? true : false;
-            return result;
+            return int.TryParse(TryAgainForm, out int numtryagainform) && numtryagainform >= 1 && numtryagainform <= 2 ? true : false;
         }
 
         public static bool validNumber(this string valid_Number)
@@ -46,31 +38,18 @@ namespace ProjectForm.Class
         }
         public static string Checkphone(string check_number)
         {
-            if (check_number.StartsWith("09"))
-            {
-                return check_number;
-            }
-            else if (check_number.StartsWith("+989"))
-            {
-                return check_number.Replace("+98", "0");
-            }
-            else if (check_number.StartsWith("9"))
-            {
-                return "0" + check_number;
-            }
-            {
-                return "false";
-            }
+            if (check_number.StartsWith("09")) return check_number;
+            if (check_number.StartsWith("+989")) return check_number.Replace("+98", "0");
+            if (check_number.StartsWith("9")) return "0" + check_number;
+            return "false";
         }
         public static bool validgender(this string gender)
         {
-            bool result = gender == "Male" || gender == "Famle" || gender == "Other" || gender == "male" || gender == "famle" || gender == "other" ? true : false;
-            return result;
+            return gender == "Male" || gender == "Famle" || gender == "Other" || gender == "male" || gender == "famle" || gender == "other" ? true : false;
         }
         public static bool mustGoToMiliteryService(string gender, int age)
         {
-            bool result = (gender == "male" || gender == "Male") && age >= 18 && age <= 50 ? true : false;
-            return result;
+            return (gender == "male" || gender == "Male") && age >= 18 && age <= 50 ? true : false;
         }
     }
 }
